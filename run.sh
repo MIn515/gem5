037a899fb6a07fe8d419c882afdad427aabef30d
MIn515 <846700248@qq.com>
2024-07-07 08:34:43 -0700

    the version of gem5 that can pass api

--- run.sh
+++ run.sh
@@ -16,6 +16,12 @@
 # delete result of scons :
 # scons -c
 
+#cp /home/xtee/mytee/dev-keystone/keystone-lastcmakeversion/build/sm.build/platform/generic/firmware/fw_payload.elf /home/xtee/mytee/FIMI-TEE/gem5/image/fs/fw_payload.elf
+
+#cp /home/xtee/mytee/dev-keystone/keystone-lastcmakeversion/build/buildroot.build/images/rootfs.ext2  /home/xtee/mytee/FIMI-TEE/gem5/image/fs/rootfs.ext2
+
+#./build/RISCV/gem5.opt configs/example/riscv/fs_linux.py --kernel=/home/xtee/mytee/FIMI-TEE/gem5/image/fs/fw_payload.elf --disk-image=/home/xtee/mytee/FIMI-TEE/gem5/image/fs/rootfs.ext2 --cpu-type=MinorCPU -n=4 --mem-size=512MB --l1d_size=64kB --l1i_size=32kB --caches
+
 ## run
 
 if [ $# -eq 0 ]; then
@@ -30,13 +36,17 @@ GEM5_DIR=`pwd`;
 
 case "$1" in
     se)
-        ./build/RISCV/gem5.opt configs/example/se.py --cmd=${GEM5_DIR}/image/se/test
+        ./build/RISCV/gem5.opt configs/example/se.py --cmd=${GEM5_DIR}/image/se/eapp-fakesys
         ;;
     fs)
-        ./build/RISCV/gem5.opt configs/example/riscv/fs_linux.py --kernel=${GEM5_DIR}/image/fs/fw_payload.elf --disk-image=${GEM5_DIR}/image/fs/rootfs.ext2 --cpu-type=AtomicSimpleCPU -n=2
+        # ./build/RISCV/gem5.opt configs/example/riscv/fs_linux.py --kernel=${GEM5_DIR}/image/fs/fw_payload.elf --disk-image=${GEM5_DIR}/image/fs/rootfs.ext2 --cpu-type=AtomicSimpleCPU -n=2
+
+	./build/RISCV/gem5.opt configs/example/riscv/fs_linux.py --kernel=${GEM5_DIR}/image/fs/fw_payload.elf --disk-image=${GEM5_DIR}/image/fs/rootfs.ext2 --cpu-type AtomicSimpleCPU --cpu-clock 2GHz -n 1 --mem-size 2GB 
+
+	# ./build/RISCV/gem5.opt configs/example/riscv/fs_linux.py --kernel=${GEM5_DIR}/image/fs/fw_payload.elf --disk-image=${GEM5_DIR}/image/fs/rootfs.ext2 --cpu-type=O3CPU -n=4 --mem-size 512MB --caches
         ;;
     *)
-        echo "错误：不支持的参数。" >&2
+        echo "error：unsupported arguments。" >&2
         exit 1
         ;;
 esac
